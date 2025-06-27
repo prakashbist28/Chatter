301b2aab611c00b7a29a85fef08228882c678092
Prakash Bist <prakashcena540@gmail.com>
2025-06-27 12:52:18 +0530

    Update APIroutes.js

--- public/src/utils/APIroutes.js
+++ public/src/utils/APIroutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://chatter-back.onrender.com";
-export const host = "http://localhost:5000";
+export const host = "https://chatter-back.onrender.com";
+
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const SetAvatarRoute = `${host}/api/auth/setavatar`
